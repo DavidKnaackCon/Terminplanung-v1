0524b67b729cccde1914b6c81387e5e8dd6039f4
DavidKnaack <mastercontax@gmx.de>
2026-07-06 10:29:27 +0200

    Initial Django Projekt Setup

---
+++ terminplaner/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for terminplaner project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'terminplaner.settings')
+
+application = get_asgi_application()
