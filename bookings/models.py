cc982fa8b006c8e9c74eeed5e2277c17161b5de0
DavidKnaack <mastercontax@gmx.de>
2026-07-13 14:05:08 +0200

    bla

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    vorname = models.CharField(max_length=100)
+    nachname = models.CharField(max_length=100)
+    geburtsdatum = models.DateField()
+    telefon = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.vorname + " " + self.nachname
+
+
+class Termin(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    datum = models.DateField()
+    uhrzeit = models.TimeField()
+    notiz = models.TextField(blank=True)
+
+    def __str__(self):
+        return str(self.datum) + " - " + str(self.patient)
